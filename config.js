8991e7bba36bd9ac06e679b60ce0d689622f81e5
Akanksha Nilosey <nilosey.akanksha@gmail.com>
2018-12-08 23:40:34 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+                'invokeUrl': 'https://fl8inowho4.execute-api.us-east-1.amazonaws.com/apideploy/search',
+                'putUrl': 'http://publicaccessbucket1.s3-website-us-east-1.amazonaws.com',
+                'apiKey': 'SkAK8z6JOOlsd0aevMTB31tbal44IBY9QlxDhSs3',
+                'region': 'us-east-1' ,
+                "accessKey":"AKIAIH7XHVGAAENTP54Q",
+                "secretKey":"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"
+              }
