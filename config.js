4a82336b3f33e7226d2e64645a5d15394d56a43a
Akanksha Nilosey <nilosey.akanksha@gmail.com>
2019-02-12 18:05:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var config = {
                 'putUrl': 'http://publicaccessbucket1.s3-website-us-east-1.amazonaws.com',
                 'apiKey': 'SkAK8z6JOOlsd0aevMTB31tbal44IBY9QlxDhSs3',
                 'region': 'us-east-1' ,
-                "accessKey":"AKIAIH7XHVGAAENTP54Q",
-                "secretKey":"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"
+                "accessKey":"",
+                "secretKey":""
               }
